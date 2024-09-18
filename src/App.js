839ce489ac876bb85c6b97a7481934f6cd577a5c
Kalash Kumari Thakur <152004250+Kkt04@users.noreply.github.com>
2024-09-18 11:56:37 +0530

    m

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import './components/MoviesApp.js';
 import MoviesApp from './components/MoviesApp.js';
 
+
 function App() {
   return (
     <div className="App">
--- src/components/MoviesApp.js
+++ src/components/MoviesApp.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import './MoviesApp.css';
 import { AiOutlineSearch } from "react-icons/ai";
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 export default function MoviesApp() {
     const [movies, setMovies] = useState([]);
@@ -9,7 +10,7 @@ export default function MoviesApp() {
     const [sortBy, setSortBy] = useState('popularity.desc');
     const [selectedGenre, setSelectedGenre] = useState('');
     const [genres, setGenres] = useState([]);
-    const [expandedMovieId, setExpandedMovieId] = useState(null); // Corrected variable name
+    const [expandedMovieId, setExpandedMovieId] = useState(null); 
 
     useEffect(() => {
         const fetchGenres = async () => {
@@ -55,7 +56,7 @@ export default function MoviesApp() {
 
     const handleSearchChange = (event) => {
         setSearchQuery(event.target.value);
-    };
+      };
     const handleSortChange = (event) => {
         setSortBy(event.target.value);
     };
@@ -68,62 +69,65 @@ export default function MoviesApp() {
     };
 
     return (
-        <div>
-            <h1>MoviesHub</h1>
-            <div className='search-bar'>
+            <div>
+                <div className="header">
+                <h1>Movie<span style={{ backgroundColor: 'orange', borderRadius: '20px' }}>Hub</span></h1>
+                </div>
+                <div className='search-bar'>
                 <input 
                     type='text' 
                     placeholder='Search for a movie' 
                     value={searchQuery} 
                     onChange={handleSearchChange} 
                     className='search-input' 
-                />
-                <button className='search-button'>
-                    <AiOutlineSearch />
-                </button>
-            </div>
-            <div className='filters'>
-                <label htmlFor='sort-by'>Sort By:</label>
-                <select id='sort-by' value={sortBy} onChange={handleSortChange}>
-                    <option value="popularity.desc">Popularity Descending</option>
-                    <option value="popularity.asc">Popularity Ascending</option>
-                    <option value="vote_average.asc">Rating Ascending</option>
-                    <option value="vote_average.desc">Rating Descending</option>
-                    <option value="release_date.desc">Release Date Descending</option>
-                    <option value="release_date.asc">Release Date Ascending</option>
-                </select>
-                
-                <label htmlFor='genre'>Genre:</label>
-                <select id='genre' value={selectedGenre} onChange={handleGenreChange}>
-                    <option value="">All Genres</option>
-                    {genres.map((genre) => (
-                        <option key={genre.id} value={genre.id}>
-                            {genre.name}
-                        </option>
+                    />
+                    <button className='search-button'>
+                        <AiOutlineSearch />
+                    </button>
+                </div>
+                <div className='filters'>
+                    <label htmlFor='sort-by'>Sort By:</label>
+                    <select id='sort-by' value={sortBy} onChange={handleSortChange}>
+                        <option value="popularity.desc">Popularity Descending</option>
+                        <option value="popularity.asc">Popularity Ascending</option>
+                        <option value="vote_average.asc">Rating Ascending</option>
+                        <option value="vote_average.desc">Rating Descending</option>
+                        <option value="release_date.desc">Release Date Descending</option>
+                        <option value="release_date.asc">Release Date Ascending</option>
+                    </select>
+                    
+                    <label htmlFor='genre'>Genre:</label>
+                    <select id='genre' value={selectedGenre} onChange={handleGenreChange}>
+                        <option value="">All Genres</option>
+                        {genres.map((genre) => (
+                            <option key={genre.id} value={genre.id}>
+                                {genre.name}
+                            </option>
+                        ))}
+                    </select>
+                </div>
+                <div className='movies-wrapper'>
+                    {movies.map((movie) => (
+                        <div key={movie.id} className='movie'>
+                            <img 
+                                src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} 
+                                alt={movie.title} 
+                                className='movie-poster'
+                            />
+                            <h2>{movie.title}</h2>
+                            <p className='rating'>Rating: {movie.vote_average}</p>
+                            {expandedMovieId === movie.id ? (
+                                <p>{movie.overview}</p>
+                            ) : (
+                                <p>{movie.overview.substring(0, 150)}...</p>
+                            )}
+                            <button onClick={() => toggleDescription(movie.id)} className='read-more'>
+                                {expandedMovieId === movie.id ? 'Read Less' : 'Read More'}
+                            </button>
+                        </div>
                     ))}
-                </select>
+                </div>
             </div>
-            <div className='movies-wrapper'>
-                {movies.map((movie) => (
-                    <div key={movie.id} className='movie'>
-                        <img 
-                            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} 
-                            alt={movie.title} 
-                            className='movie-poster'
-                        />
-                        <h2>{movie.title}</h2>
-                        <p className='rating'>Rating: {movie.vote_average}</p> {/* Fixed rating value */}
-                        {expandedMovieId === movie.id ? (
-                            <p>{movie.overview}</p>
-                        ) : (
-                            <p>{movie.overview.substring(0, 150)}...</p>
-                        )}
-                        <button onClick={() => toggleDescription(movie.id)} className='read-more'>
-                            {expandedMovieId === movie.id ? 'Read Less' : 'Read More'}
-                        </button>
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
+            
+        );
 }
